b5458bd041771a64704fbcdff448fd7c8bfee9bb
DanielleWashington <101582095+DanielleWashington@users.noreply.github.com>
2023-10-18 12:06:06 -0400

    22 restructure root module (#23)
    * added root module notes
    
    * restructure root module

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-  cloud {
-    organization = "dwashington100292"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-    source = "hashicorp/aws"
-    version = "5.19.0"
-    }
-  }
-}
-
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
@@ -40,9 +12,8 @@ resource "aws_s3_bucket" "example" {
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
   bucket = random_string.bucket_name.result
-}
 
-
-output "random_bucket_name_result" {
-  value = random_string.bucket_name.result
+  tags = {
+    Useruuid        = "var.user_uuid"
+  }
 }
