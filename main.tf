1beb24866903e55a739fe5891f653dcb57a8ddb5
DanielleWashington <101582095+DanielleWashington@users.noreply.github.com>
2023-12-07 20:29:13 -0500

    Revert "Daniellewashington/s static website 28 (#29)"
    This reverts commit 621f66b16bfa4eaae1005444089b143feb39b292.

--- main.tf
+++ main.tf
@@ -7,21 +7,18 @@ terraform {
   #    name = "terra-house-1"
   #  }
   #}
-  # cloud {
-  #   organization = "dwashington100292"
+  cloud {
+    organization = "dwashington100292"
 
-  #   workspaces {
-  #     name = "terra-house-1"
-  #   }
-  # } 
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
 
 
 }
-
 module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
-  error_html_filepath = var.error_html_filepath
-  index_html_filepath = var.index_html_filepath
 }
--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.bucket_name
 }
-
-output "s3_website_endpoint" {
-  description = "S3 Static website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
