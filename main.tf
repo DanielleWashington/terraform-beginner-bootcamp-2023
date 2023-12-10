c522261ffc76b87806c0253a2bb1f5d41063666d
Danielle Washington <dwashington.100292@gmail.com>
2023-12-10 00:24:10 +0000

    module fix

--- main.tf
+++ main.tf
@@ -22,11 +22,6 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
-<<<<<<< HEAD
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
-=======
-  error_html_filepath = var.error_html_filepath
-  index_html_filepath = var.index_html_filepath
->>>>>>> f1f3272f15d82ea0ffd5084ac245fd3c552708c4
 }
