f1f3272f15d82ea0ffd5084ac245fd3c552708c4
DanielleWashington <101582095+DanielleWashington@users.noreply.github.com>
2023-12-09 19:18:49 -0500

    S3 Static Website Hosting #32 enabled static website hosting, uploaded error and index file via TF (#33)

--- variables.tf
+++ variables.tf
@@ -5,3 +5,13 @@ variable "user_uuid" {
 variable "bucket_name" {
   type = string
 }
+
+variable "index_html_filepath" {
+  description = "File path to the index.html file"
+  type        = string
+}
+
+variable "error_html_filepath" {
+  description = "File path to the error.html file"
+  type        = string
+}
