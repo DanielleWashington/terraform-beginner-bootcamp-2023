a379f94e2870e8bb6a3ef9a7951203d7488e5148
Danielle Washington <dwashington.100292@gmail.com>
2024-01-27 02:53:37 +0000

    Tested terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -25,16 +25,30 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid= var.teacherseat_user_uuid
+  token=var.terratowns_access_token
   
 }
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-# }
+
+resource "terratowns_home" "home" {
+  name = "Bossa Nova for the World!"
+  description = <<DESCRIPTION
+Bossa nova, a music style created in Brazil, quite literally means 
+"new wave." It is known for its smooth, elegant melodies that is a 
+blend of samba and jazz. It originated in Rio de Janeiro's Copacabana neighborhood. 
+DESCRIPTION
+ domain_name = module.terrahouse_aws.cloudfront_url
+ town = "missingo"
+ content_version = 1
+}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+
+}
